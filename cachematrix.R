bc13037516af937943b8ccfb7b2b401a9e0fd60e
Yves Stalgies <ys@stalgies.de>
2014-11-19 11:56:32 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## create matrix
 makeCacheMatrix <- function(x = matrix()) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y											# set x out of the local scope
+    s <<- NULL										# make sure there is no s in the global scope
   }
+  get <- function() x
+  setinverse <- function(inverse) s <<- inverse		# set the inverse out of the local scope
+  getinverse <- function() s
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
 
 
 ## Function returns the inverse of matrix 'x'
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 
+  s <- x$getinverse()
+  if(!is.null(s)) {						## the inverse was cached before
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()						## inverse has to be calculated
+  s <- solve(data)						## calculate it
+  x$setinverse(s)						## set the inverse
+  s
 }
